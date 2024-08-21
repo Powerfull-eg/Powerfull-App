2b575b3514cfa567e50246ab9b237e90508cc21c
AbdallaHawwas <abdallahawwas9@gmail.com>
2024-08-21 23:31:43 +0300

    Finish almost design till batteries part

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,11 +6,11 @@
       <div class="offcanvas-header">
         <div class="top d-flex justify-content-between w-100 pb-3">
           <!-- shop image -->
-          <div class="shop-logo" style="flex-basis: 20%;width: 80px;height: 80px;">
-            <img src="assets/images/powericon.png" class='d-block mx-auto mb-3 w-50'>
+          <div class="shop-logo" style="flex-basis: 20%; width: 100%; position: relative">
+            <img src="assets/images/powericon.png" style="position: absolute; object-fit: contain; top: 10%; width: 75%; height: 75%;">
           </div>
           <!-- name and summary data -->
-          <div class="shop-info" style="flex-basis: 80%;">
+          <div class="shop-info" style="flex-basis: 100%;">
             <!-- name -->
             <div class="shop-name" style="flex-basis: 70%;display: grid;">
               <div class="d-flex elipsis">
@@ -18,62 +18,125 @@
               </div>
             </div>
             <!-- summary data -->
-            <div class="summary d-flex flex-row">
+            <div class="summary d-flex flex-row justify-content-start">
               <!-- distance -->
-              <div class="distance" style="padding: 0 10px 0 0;">
-                <img src="assets/icons/telegram.svg" style=" width: 20px;" alt="distance">
+              <div class="distance" style="padding-right: 5px;">
+                <img src="assets/icons/telegram.svg" style=" width: 1rem" alt="distance">
                 <span>1.5 km</span>
               </div>
               <!-- time -->
-              <div class="timing d-flex flex-row" style="padding: 0 10px 0 0;">
-                <img src="assets/icons/time.svg" style=" width: 20px;" alt="distance">
-                <span class="fs-6 font-weight-bold" style="color: #00ff00; padding: 0 10px 0 0;"> مفتوح </span>
-                <span class="fs-6" style="padding: 0 10px 0 0;">22:00 يغلق</span>
+              <div class="timing d-flex flex-row" style="padding: 0 5px 0 0;">
+                <img src="assets/icons/time.svg" style=" width: 1rem;" alt="distance">
+                <span class="font-weight-bold" style="color: #00ff00; padding: 0 5px 0 0;"> مفتوح </span>
+                <span>22:00 يغلق</span>
               </div>
               <!-- device status -->
-              <div class="device-status" style="padding: 0 10px 0 0;">
+              <div class="device-status">
                 <div class="font-weight-bold" style="padding:3px; background-color: #8ac78a;"> 
                   <span style="border-radius: 50%; height: 100%;" class="bg-success d-block"></span>
-                  <span style="font-size: 12px;">جاهز للإستخدام</span>
+                  <span>جاهز للإستخدام</span>
                 </div>
               </div>
             </div>
           </div>
           <!-- close -->
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <button type="button" style="position: absolute; top: 20px; right: 10px;" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <!-- <h5 class="offcanvas-title" id="offcanvasBottomLabel">{{modalData.shopName}}</h5> -->
       </div>
-      <div class="offcanvas-body small">
-        <div class="status d-flex">
-          <img src="assets/icons/battery-full.svg" style="padding: 0 10px 0 0;" width="40" alt="battery-full">
-          <span :class="'text text-' + (modalData.online ? 'success' : 'danger')" style="margin-top: 10px;font-weight: 400; font-weight:bold;">{{modalData.online ? " Online" : " Offline"}}</span>
-        </div>
-        <div class="address">
-          {{ modalData.address }}
-        </div>
-        <div class="working-hours" style="margin-top: 10px;">
-          <span>Working Hours: </span>
-          <span class="text-success" style="margin-top: 10px;font-weight: 600;">10:00 - 22:00</span>
-        </div>
-        <div class="prices">
-          <span>Price: {{ modalData.prices }}</span>
-        </div>
-        <div class="slots" v-if="modalData.online">
-          <div class="d-flex justify-content-between">
-            <span class="text text-success font-weight-bold">Full Slots: {{ modalData.slots.busySlots }}</span>
-            <span class="text text-danger font-weight-bold">Empty Slots: {{ modalData.slots.emptySlots }}</span>
+      <div class="offcanvas-body small middle">
+        <!-- Body Top Part -->
+        <div class="body-top d-flex justify-content-between w-100 px-3" style="height: 50px;">
+          <!-- Access -->
+          <div class="access d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <div class="d-flex flex-column">
+              <span>صلاحية الوصول</span>
+              <span>مكان عام</span>
+            </div>
+          </div>
+          <!-- Type -->
+          <div class="type d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <div class="d-flex flex-column">
+              <span>النوع</span>
+              <span>مول</span>
+            </div>
+          </div>
+          <!-- Price -->
+          <div class="price d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <div class="d-flex flex-column">
+              <span>السعر</span>
+              <span>25 EGP/hr</span>
+            </div>
           </div>
         </div>
-        <div class="d-flex justify-content-evenly" v-if='modalData.online'>
-              <a target="_blank" :href="modalData.location" class="mapsBtn">Maps</a>
-              <router-link  class="qrBtn" :to="{name: 'scan'}" @click.prevent="openScanner">
-                  Scan QR Code
-              </router-link>
+        <!-- Body middle Part -->
+        <div class="body-middle d-flex justify-content-between w-100 p-3" style="height: 50px;">
+          <!-- qr scan button -->
+          <div class="qr-scan d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <span>امسح رمز الإستجابة</span>
+          </div>
+          <!-- location btn -->
+          <div class="location d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <span>الموقع</span>
+          </div>          
+          <!-- Save shop -->
+          <div class="save d-flex">
+            <img src="assets/icons/telegram.svg" alt="distance">
+            <span>حفظ</span>
+          </div>
         </div>
-        <div v-else>
-          <img src="/assets/icons/fail.png" width="100" alt=""  class="d-block mx-auto">
-          <span class="d-block text text-danger" style="font-size: 20px; font-weight: bold;text-align: center; margin-top: 15px;">Device isn't online</span>
+        <!-- Body bottom Part -->
+        <div class="body-bottom">
+          <!-- Navigator -->
+          <div class="navs d-flex justify-content-between align-items-center mx-auto px-3">
+            <!-- battery-nav -->
+            <div class="battery-nav selected">
+              <span>البطارية</span>
+              &nbsp;
+              <span class="battery-live" style="background-color: var(--background); padding: 0 4px; color: var(--color);">4</span>
+            </div>
+            <!-- menu-nav -->
+            <div class="menu-nav">
+              <span>المنيو&الخصومات</span>
+            </div>          
+            <!-- rate-nav -->
+            <div class="rate-nav">
+              <span>التقييم</span>
+            </div>
+          </div>
+        </div>
+        <!-- Bottom Content -->
+        <div class="bottom-content">
+          <!-- Batteries -->
+          <div id="batteries" class="batteries p-3">
+            <div class="d-flex flex-nowrap justify-content-evenly">
+              <div class="battery" v-for="i in 6" :key="i" :style="(i < 5 ? 'background: var(--background);':'')">
+                <span>&nbsp;</span>
+              </div>
+            </div>
+            <!-- Batteries Data-->
+            <div class="batteries-data d-flex justify-content-between w-100 pt-3">
+              <!-- Filled Batteries -->
+              <div class="filled-batteries d-flex align-items-center justify-content-center">
+                <img src="assets/icons/telegram.svg" style="width: 2rem;" alt="Filled Battery">
+                <span>المنافذ الممتلئة</span>
+                <span style="color: var(--background);color: var(--background);font-size: 2rem;padding: 0 3px;margin-top: -10px;"> . </span>
+                <span>4</span>
+              </div>
+              <!-- Empty Batteries -->
+              <div class="empty-batteries d-flex align-items-center justify-content-center">
+                <img src="assets/icons/telegram.svg" style="width: 2rem;" alt="Empty Battery">
+                <span>المنافذ الفارغة</span>
+                <span style="color: var(--background);color: var(--background);font-size: 2rem;padding: 0 3px;margin-top: -10px;"> . </span>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -312,4 +375,81 @@ export default {
       border-bottom-left-radius: 10px;
       border-bottom-right-radius: 10px;
     }
+
+    .summary {
+      font-size: .7rem;
+    }
+    .body-top  img, .body-middle img
+    {
+      width: 1.3rem;
+      height: 1.3rem;
+      padding-right: 5px;
+    }
+    .body-top  > div, .body-middle  > div
+    {
+    font-size: .6rem;
+    color: #474747;
+    flex-basis: 30%;
+    align-items: center;
+    }
+    .body-top  > div > div > span:first-of-type,.body-middle {
+      font-weight: 900;
+    }
+    .body-middle {
+      font-size: .7rem !important;
+      max-height: 30px;
+    }
+    .qr-scan {
+      background: var(--background);
+      border-top-right-radius: 10px;
+      color: var(--color) !important;
+    }
+    .body-bottom
+    {
+      border: 1px solid #474747a1;
+      border-radius: 15px;
+      font-weight: bold;
+      font-size: .7rem;
+      padding: 5px 10px;
+      width: 90%;
+      margin: 0 auto;
+    }
+    .battery-nav {
+      display: flex;
+      justify-content: space-between;
+    }
+    .navs .battery-nav.selected {
+      background-color: #dadada;
+      color: var(--background);
+      padding: 10px 15px;
+      border-radius: 15px;
+    }
+    .battery {
+      height: 6rem;
+      width: 2rem;
+      background: #dadada;
+      display: flex;
+      align-items: center;
+      border-top-right-radius: 10px;
+      border-top-left-radius: 10px;
+      border-bottom-right-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
+
+    .battery span {
+      height: 2rem;
+      width: 5px;
+      display: block;
+      text-align: center;
+      background: var(--color);
+      margin: 0 auto;
+    }
+    @media (min-width: 768px) {
+    .summary {
+      font-size: 2rem;
+    }
+    .summary img {
+      width: 2rem !important;
+    }
+}
 </style>
